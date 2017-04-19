47e09b72924c5f9d4a39834d063b9f968d0635f7
Informhunter <informhunter@gmail.com>
2017-04-19 21:12:52 +0300

    Minor fixes. Nothing works yet

--- Android/DataCollector/app/src/main/java/io/informhunter/datacollector/MainTabActivity.java
+++ Android/DataCollector/app/src/main/java/io/informhunter/datacollector/MainTabActivity.java
@@ -114,8 +114,6 @@ public class MainTabActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.tab_collect, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
