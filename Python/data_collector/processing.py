631f6b80f310cd86c7b9d37d20dad91ba8903244
Informhunter <informhunter@gmail.com>
2017-04-29 22:15:53 +0300

    Started implementing of data export/import.

--- Python/data_collector/processing.py
+++ Python/data_collector/processing.py
@@ -1,3 +1,4 @@
+import numpy as np
 import csv
 from peewee import SqliteDatabase
 from models import CaptureSession, RSSIValue, Position
@@ -32,3 +33,62 @@ def save_data_to_db(rssi_file, position_file, session_file):
             pos.timestamp = int(row[2])
             pos.capture_session = sess
             pos.save()
+
+def export_data_from_db(session_name):
+    session = (CaptureSession.select()
+               .where(CaptureSession.name == session_name)).get()
+
+    with open(session_name + '_rssi.txt', 'w') as f:
+        f.write('UUID,Major,Minor,RSSI,Timestamp\n')
+        for rv in session.rssi_values:
+            f.write("{},{},{},{},{}\n".format(
+                rv.beacon_uuid,
+                rv.beacon_major,
+                rv.beacon_minor,
+                rv.rssi,
+                rv.timestamp
+            ))
+
+    with open(session_name + '_position.txt', 'w') as f:
+        f.write('PosX,PosY,Timestamp\n')
+        for pos in session.positions:
+            f.write("{},{},{}\n".format(
+                pos.x,
+                pos.y,
+                pos.timestamp
+            ))
+
+
+def apply_smoothing_to_rssi_values(data):
+    return data
+
+def build_rssi_pos(session_names=[]):
+    sessions = (CaptureSession.select()
+                .where(CaptureSession.name << session_names))
+
+    data = []
+    for session in sessions:
+        processed_rssi = apply_smoothing_to_rssi_values(session.rssi_values)
+        i = 0
+        for current in range(1, len(session.positions)):
+            start_t = session.positions[current - 1].timestamp
+            end_t = session.positions[current].timestamp
+            delta_t = end_t - start_t
+            
+            start_x = session.positions[current - 1].x
+            end_x = session.positions[current].x
+            delta_x = end_x - start_x
+            
+            start_y = session.positions[current - 1].y
+            end_y = session.positions[current].y
+            delta_y = end_y - start_y
+            
+            while processed_rssi[i].timestamp <= end_t:
+                px = start_x + delta_x * (session.rssi_values[i].timestamp - start_t) / float(delta_t)
+                py = start_y + delta_y * (session.rssi_values[i].timestamp - start_t) / float(delta_t)
+                data.append((
+                    processed_rssi[i].beacon_uuid,
+                    processed_rssi[i].beacon_major,
+                    processed_rssi[i].beacon_minor,
+                    processed_rssi[i].rssi, px, py))
+                i += 1
