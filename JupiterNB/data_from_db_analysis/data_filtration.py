58d2e824542df7424645183936d114d0fea1f3f2
Informhunter <informhunter@gmail.com>
2017-05-15 20:37:22 +0300

    Organiazed data visualisation and filtration.

---
+++ JupiterNB/data_from_db_analysis/data_filtration.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from statsmodels.nonparametric.smoothers_lowess import lowess
+from scipy.signal import medfilt
+
+#Filter RSSI values with lowess filter
+
+def rssi_lowess_filter(df):
+    smoothed = lowess(df.RSSI, df.Timestamp)
+    new_df = df.copy()
+    new_df.RSSI = smoothed[:, 1]
+    new_df.Timestamp = smoothed[:, 0]
+    return new_df
+
+#Create median filter function with desired kernel size
+
+def create_rssi_median_filter(kernel=9):
+    def median_filter(df):
+        smoothed = medfilt(df.RSSI, kernel)
+        new_df = df.copy()
+        new_df.RSSI = smoothed
+        return new_df
+    return median_filter
+
+#Filter session RSSI values for each minor with desired filter
+
+def filter_session(sess_df, filter_func=rssi_lowess_filter):
+    result = pd.DataFrame()
+    for minor in sorted(sess_df.Minor.unique()):
+        result = result.append(
+				filter_func(sess_df[sess_df.Minor == minor])
+			)
+    return result
+
+#Filter RSSI values for each minor with desired filter in
+#DataFrame with several sessions
+
+def filter_rssi_df(df, filter_func=rssi_lowess_filter):
+    result = pd.DataFrame()
+    for sess_id in sorted(df.SessId.unique()):
+        result = result.append(
+				filter_session(df[df.SessId == sess_id])
+			)
+    return result
---
+++ JupiterNB/data_from_db_analysis/data_visualisation.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from data_filtration import filter_rssi_df
+
+
+def plot_over_image(img, x, y, **kwargs):
+    scaleX = img.shape[1] / 13.9
+    scaleY = img.shape[0] / 7.35
+    plt.imshow(img)
+    plt.plot(x * scaleX, y * scaleY, **kwargs)o
+
+def scatter_over_image(img, x, y, **kwargs):
+    scaleX = img.shape[1] / 13.9
+    scaleY = img.shape[0] / 7.35
+    plt.imshow(img)
+    plt.scatter(x * scaleX, y * scaleY, **kwargs)
+
+#Plot RSSI over time for all minors
+
+def plot_rssi_data(df, figsize=(15, 10), filter_func=None):
+    min_time = df.Timestamp.min()
+    unique_minors = sorted(df.Minor.unique())
+    fig = plt.figure(figsize=figsize)
+    for i, minor in enumerate(unique_minors):
+        if filter_func != None:
+            bd = filter_rssi_df(df[df.Minor == minor], filter_func)
+        else:
+            bd = df[df.Minor == minor]
+        ax = plt.subplot(int(np.ceil(len(unique_minors) / 3)), 3, 1 + i)
+        ax.set_title(str(minor))
+        plt.plot(bd.Timestamp-min_time, bd.RSSI)
+
+#Plot data points for all minors at their estimated location
+#with color dependent on RSSI
+
+def plot_rssi_map(df, figsize=(15, 10), filter_func=None):
+    min_time = df.Timestamp.min()
+    unique_minors = sorted(df.Minor.unique())
+    fig = plt.figure(figsize=figsize)
+    for i, minor in enumerate(unique_minors):
+        if filter_func != None:
+            bd = filter_rssi_df(df[df.Minor == minor], filter_func)
+        else:
+            bd = df[df.Minor == minor]
+        ax = plt.subplot(int(np.ceil(len(unique_minors) / 3)), 3, 1 + i)
+        ax.set_title(str(minor))
+        plt.scatter(bd.X, -bd.Y, c=-bd.RSSI, cmap='Greys')
+
+#Plot route, RSSI graphs and RSSI maps
+
+def plot_session(df, filter_func=lambda x: x):
+    plt.title("Route")
+    plt.scatter(df.X, -df.Y)
+    plot_rssi_data(df, filter_func=filter_func)
+    plot_rssi_map(df, filter_func=filter_func)
