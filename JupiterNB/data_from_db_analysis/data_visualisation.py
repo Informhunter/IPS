3f19b120f952a45b147e65f91da7746a5dfa5309
Informhunter <informhunter@gmail.com>
2017-05-15 21:39:39 +0300

    Fixed mistakes. Visualisation works.

--- JupiterNB/data_from_db_analysis/data_visualisation.py
+++ JupiterNB/data_from_db_analysis/data_visualisation.py
@@ -7,7 +7,7 @@ def plot_over_image(img, x, y, **kwargs):
     scaleX = img.shape[1] / 13.9
     scaleY = img.shape[0] / 7.35
     plt.imshow(img)
-    plt.plot(x * scaleX, y * scaleY, **kwargs)o
+    plt.plot(x * scaleX, y * scaleY, **kwargs)
 
 def scatter_over_image(img, x, y, **kwargs):
     scaleX = img.shape[1] / 13.9
@@ -33,7 +33,7 @@ def plot_rssi_data(df, figsize=(15, 10), filter_func=None):
 #Plot data points for all minors at their estimated location
 #with color dependent on RSSI
 
-def plot_rssi_map(df, figsize=(15, 10), filter_func=None):
+def plot_rssi_map(df, flat_img, figsize=(15, 10), filter_func=None):
     min_time = df.Timestamp.min()
     unique_minors = sorted(df.Minor.unique())
     fig = plt.figure(figsize=figsize)
@@ -44,12 +44,12 @@ def plot_rssi_map(df, figsize=(15, 10), filter_func=None):
             bd = df[df.Minor == minor]
         ax = plt.subplot(int(np.ceil(len(unique_minors) / 3)), 3, 1 + i)
         ax.set_title(str(minor))
-        plt.scatter(bd.X, -bd.Y, c=-bd.RSSI, cmap='Greys')
+        scatter_over_image(flat_img, bd.X, bd.Y, c=-bd.RSSI, cmap='Greys')
 
 #Plot route, RSSI graphs and RSSI maps
 
-def plot_session(df, filter_func=lambda x: x):
+def plot_session(df, flat_img, filter_func=lambda x: x):
     plt.title("Route")
-    plt.scatter(df.X, -df.Y)
+    scatter_over_image(flat_img, df.X, df.Y)
     plot_rssi_data(df, filter_func=filter_func)
-    plot_rssi_map(df, filter_func=filter_func)
+    plot_rssi_map(df, flat_img, filter_func=filter_func)
