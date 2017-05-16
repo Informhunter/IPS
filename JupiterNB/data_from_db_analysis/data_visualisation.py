176b3dcac9fa08ad53b6ab26a033b004afb93e7b
Informhunter <informhunter@gmail.com>
2017-05-16 04:48:41 +0300

    Added beacon position display.

--- JupiterNB/data_from_db_analysis/data_visualisation.py
+++ JupiterNB/data_from_db_analysis/data_visualisation.py
@@ -1,6 +1,22 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from data_filtration import filter_rssi_df
+
+#Plot some graph in image coordinates
+
+def plot_in_image(img, x, y, **kwargs):
+    scaleX = img.shape[1] / 13.9
+    scaleY = img.shape[0] / 7.35
+    plt.plot(x * scaleX, y * scaleY, **kwargs)
+
+#Scatter some graph in image coordinates
+
+def scatter_in_image(img, x, y, **kwargs):
+    scaleX = img.shape[1] / 13.9
+    scaleY = img.shape[0] / 7.35
+    plt.scatter(x * scaleX, y * scaleY, **kwargs)
+
 
 #Plot some graph over image of flat plan
 
@@ -26,33 +42,48 @@ def plot_rssi_data(df, figsize=(15, 10), filter_func=None):
     fig = plt.figure(figsize=figsize)
     for i, minor in enumerate(unique_minors):
         if filter_func != None:
-            bd = filter_rssi_df(df[df.Minor == minor], filter_func)
+            bd = filter_rssi_df(
+                    df[df.Minor == minor],
+                    filter_func=filter_func
+                )
         else:
             bd = df[df.Minor == minor]
         ax = plt.subplot(int(np.ceil(len(unique_minors) / 3)), 3, 1 + i)
         ax.set_title(str(minor))
+        plt.xlabel('Time')
+        plt.ylabel('RSSI')
         plt.plot(bd.Timestamp-min_time, bd.RSSI)
 
 #Plot data points for all minors at their estimated location
 #with color dependent on RSSI
 
-def plot_rssi_map(df, flat_img, figsize=(15, 10), filter_func=None):
+def plot_rssi_map(df, flat_img, figsize=(15, 10), beacons=pd.DataFrame(), filter_func=None):
     min_time = df.Timestamp.min()
     unique_minors = sorted(df.Minor.unique())
     fig = plt.figure(figsize=figsize)
     for i, minor in enumerate(unique_minors):
         if filter_func != None:
-            bd = filter_rssi_df(df[df.Minor == minor], filter_func)
+            bd = filter_rssi_df(
+                    df[df.Minor == minor],
+                    filter_func=filter_func
+                )
         else:
             bd = df[df.Minor == minor]
+
         ax = plt.subplot(int(np.ceil(len(unique_minors) / 3)), 3, 1 + i)
         ax.set_title(str(minor))
+        plt.xlabel('X')
+        plt.ylabel('Y')
         scatter_over_image(flat_img, bd.X, bd.Y, c=-bd.RSSI, cmap='Greys')
+        if not beacons.empty:
+            scatter_in_image(flat_img, beacons.X, beacons.Y)
 
 #Plot route, RSSI graphs and RSSI maps
 
-def plot_session(df, flat_img, filter_func=None):
+def plot_session(df, flat_img, beacons=pd.DataFrame(), filter_func=None):
     plt.title("Route")
     scatter_over_image(flat_img, df.X, df.Y)
+    if not beacons.empty:
+        scatter_in_image(flat_img, beacons.X, beacons.Y)
     plot_rssi_data(df, filter_func=filter_func)
-    plot_rssi_map(df, flat_img, filter_func=filter_func)
+    plot_rssi_map(df, flat_img, beacons=beacons, filter_func=filter_func)
